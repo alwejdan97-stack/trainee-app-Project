36c16f2dea17e04f74153e9d5b0954dff2dd3c95
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-02 15:19:13 +0400

    last modified

--- trainee-app/src/main/java/com/example/trainee_app/HelloController.java
+++ trainee-app/src/main/java/com/example/trainee_app/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.trainee_app;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
