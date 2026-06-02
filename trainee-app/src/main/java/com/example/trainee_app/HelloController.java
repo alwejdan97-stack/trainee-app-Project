d645ca39ae7b6e08a9fa4b5aa737fa79966270b4
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-02 14:29:44 +0400

    add special spring tags HelloController class

--- trainee-app/src/main/java/com/example/trainee_app/HelloController.java
+++ trainee-app/src/main/java/com/example/trainee_app/HelloController.java
@@ -1,4 +1,8 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class HelloController {
 }
