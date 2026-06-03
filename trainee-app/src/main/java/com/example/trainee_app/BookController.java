3b7c0e2e670231ca0dad96458584d7f91638284c
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 09:43:29 +0400

    create Arraylist inside BookController

--- trainee-app/src/main/java/com/example/trainee_app/BookController.java
+++ trainee-app/src/main/java/com/example/trainee_app/BookController.java
@@ -1,6 +1,25 @@
 package com.example.trainee_app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class BookController {
+    private static ArrayList<Book> books=new ArrayList<>();
+
+    @GetMapping("books")
+    public List<Book> books(){
+        return Arrays.asList(
+                new Book(1,"Arabic"),
+                new Book(2,"Islamic"),
+                new Book(4,"Mathematics"),
+                new Book(5,"Since"),
+                new Book(6,"Information Technology")
+        );
+    }
 }
