e40a7ed5366dd6737556fdbeb556ff8c88ac2fe9
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 11:32:03 +0400

    adding necessary REST annotation sticker

--- trainee-app/src/main/java/com/example/trainee_app/LibraryController.java
+++ trainee-app/src/main/java/com/example/trainee_app/LibraryController.java
@@ -1,4 +1,9 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LibraryController {
 }
