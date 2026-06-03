53baf0453d5bf880ebe3eaa6b41e33461d6d40d8
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 09:26:44 +0400

    declare two private variables inside Book class

--- trainee-app/src/main/java/com/example/trainee_app/Book.java
+++ trainee-app/src/main/java/com/example/trainee_app/Book.java
@@ -1,4 +1,27 @@
 package com.example.trainee_app;
 
 public class Book {
+    private int ID;
+    private String name;
+
+    public Book(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
