2a64e7056f3e0ac7df2d48e95cffdda70707ed0e
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 11:33:43 +0400

    adding authorList and bookList inside LibraryController class

--- trainee-app/src/main/java/com/example/trainee_app/LibraryController.java
+++ trainee-app/src/main/java/com/example/trainee_app/LibraryController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class LibraryController {
+    private static List<Author> authorList=new ArrayList<>();
+    private static List<Book> bookList=new ArrayList<>();
 }
