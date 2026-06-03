814af5a99a5f94243386d13d685d89506bb1c443
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 13:54:50 +0400

    Create a constructor and getter and setter and methods InventoryBook class

--- trainee-app/src/main/java/com/example/trainee_app/InventoryBook.java
+++ trainee-app/src/main/java/com/example/trainee_app/InventoryBook.java
@@ -6,4 +6,42 @@ public class InventoryBook {
     private double price;
     private int stockCount;
 
+    public InventoryBook(int bookID, String title, double price, int stockCount) {
+        this.bookID = bookID;
+        this.title = title;
+        this.price = price;
+        this.stockCount = stockCount;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockCount() {
+        return stockCount;
+    }
+
+    public void setStockCount(int stockCount) {
+        this.stockCount = stockCount;
+    }
 }
