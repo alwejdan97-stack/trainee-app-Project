3478ed5fc11322916cc6674dce1959f4d338c996
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 13:59:26 +0400

    adding necessary required annotation inside BookstoreController

--- trainee-app/src/main/java/com/example/trainee_app/BookstoreController.java
+++ trainee-app/src/main/java/com/example/trainee_app/BookstoreController.java
@@ -1,4 +1,10 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
 public class BookstoreController {
+
 }
