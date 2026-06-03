bcfab0aa2de0206642558368c16ea19e185af006
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 14:03:57 +0400

    create addInventoryBook() inside BookstoreController

--- trainee-app/src/main/java/com/example/trainee_app/BookstoreController.java
+++ trainee-app/src/main/java/com/example/trainee_app/BookstoreController.java
@@ -4,7 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class BookstoreController {
+    public static List<InventoryBook> inventoryBookList=new ArrayList<>();
 
+    @GetMapping("newInventoryBook")
+    public String addInventoryBook(@RequestParam int id, @RequestParam String name, @RequestParam double price, @RequestParam int stockCount){}
 }
