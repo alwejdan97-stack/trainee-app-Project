846e14d2e28cf1d09fb6cbeadf866e0fce2abc30
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-03 11:29:21 +0400

    update constructor, getters, and setters inside Book  class

--- trainee-app/src/main/java/com/example/trainee_app/Book.java
+++ trainee-app/src/main/java/com/example/trainee_app/Book.java
@@ -3,10 +3,12 @@ package com.example.trainee_app;
 public class Book {
     private int ID;
     private String name;
+    private String authorId;
 
-    public Book(int ID, String name) {
+    public Book(int ID, String name, String authorId) {
         this.ID = ID;
         this.name = name;
+        this.authorId = authorId;
     }
 
     public int getID() {
@@ -23,5 +25,13 @@ public class Book {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
     }
 }
